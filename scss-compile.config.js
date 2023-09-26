f20fe19542d5de107abb74555e54f68799a00e41
Saltssaumure <themes@saltssaumure.net>
2023-09-26 02:01:50 +0100

    Setup for monorepo

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -2,7 +2,7 @@
 export default {
     meta: {
         name: "EmojiReplace" /* The official name of your theme. */,
-        scss: "Apple" /* The name of your base scss file. */,
+        scss: "root/Apple" /* The name of your base scss file. */,
         repo: "EmojiReplace" /* The name of your theme repository. */,
         version: "dev" /* The version of your theme. */
     }
